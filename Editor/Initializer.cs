76c3c9cb5791d0d3d5f7d05d318a5000703e7834
Phill <philldamaskin@gmail.com>
2019-03-29 18:54:07 -0400

    Update Initializer.cs

--- Editor/Initializer.cs
+++ Editor/Initializer.cs
@@ -77,60 +77,63 @@ namespace Popcron.Referencer
                     string text = File.ReadAllText(copy);
                     string find = "LICENSE SYSTEM [";
                     int start = text.IndexOf(find);
-                    string data = text.Substring(start + find.Length, 100).Split(']')[0];
-                    string year = data.Substring(0, 4);
-                    string month = "";
-                    string day = "";
-                    string hour = "";
-                    string minute = "";
-                    string seconds = "";
-                    string rest = data.Substring(4).Split(' ')[0];
+                    if (start != -1)
+                    {
+                        string data = text.Substring(start + find.Length, 100).Split(']')[0];
+                        string year = data.Substring(0, 4);
+                        string month = "";
+                        string day = "";
+                        string hour = "";
+                        string minute = "";
+                        string seconds = "";
+                        string rest = data.Substring(4).Split(' ')[0];
 
-                    if (rest.Length == 2)
-                    {
-                        month = int.Parse(rest[0].ToString()).ToString("00");
-                        day = int.Parse(rest[1].ToString()).ToString("00");
-                    }
-                    else if (rest.Length == 3)
-                    {
-                        //if the first 2 digits are less than or equal to 12
-                        //then this is the month
-                        //otherwise its just the first digit that is the month
-                        if (int.Parse(rest.Substring(0, 2)) <= 12)
-                        {
-                            month = rest.Substring(0, 2);
-                            day = int.Parse(rest[2].ToString()).ToString("00");
-                        }
-                        else
+                        if (rest.Length == 2)
                         {
                             month = int.Parse(rest[0].ToString()).ToString("00");
-                            day = rest.Substring(1, 2);
+                            day = int.Parse(rest[1].ToString()).ToString("00");
                         }
-                    }
-                    else if (rest.Length == 4)
-                    {
-                        month = rest.Substring(0, 2);
-                        day = rest.Substring(2, 2);
-                    }
+                        else if (rest.Length == 3)
+                        {
+                            //if the first 2 digits are less than or equal to 12
+                            //then this is the month
+                            //otherwise its just the first digit that is the month
+                            if (int.Parse(rest.Substring(0, 2)) <= 12)
+                            {
+                                month = rest.Substring(0, 2);
+                                day = int.Parse(rest[2].ToString()).ToString("00");
+                            }
+                            else
+                            {
+                                month = int.Parse(rest[0].ToString()).ToString("00");
+                                day = rest.Substring(1, 2);
+                            }
+                        }
+                        else if (rest.Length == 4)
+                        {
+                            month = rest.Substring(0, 2);
+                            day = rest.Substring(2, 2);
+                        }
 
-                    hour = int.Parse(data.Split(' ')[1].Split(':')[0]).ToString("00");
-                    minute = int.Parse(data.Split(' ')[1].Split(':')[1]).ToString("00");
-                    seconds = int.Parse(data.Split(' ')[1].Split(':')[2]).ToString("00");
+                        hour = int.Parse(data.Split(' ')[1].Split(':')[0]).ToString("00");
+                        minute = int.Parse(data.Split(' ')[1].Split(':')[1]).ToString("00");
+                        seconds = int.Parse(data.Split(' ')[1].Split(':')[2]).ToString("00");
 
-                    key = Settings.UniqueIdentifier + ".InitializedLogTime";
-                    string logTimeString = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + seconds;
-                    if (EditorPrefs.GetString(key) == logTimeString) return;
+                        key = Settings.UniqueIdentifier + ".InitializedLogTime";
+                        string logTimeString = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + seconds;
+                        if (EditorPrefs.GetString(key) == logTimeString) return;
 
-                    DateTime logTime = DateTime.ParseExact(logTimeString, "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
-                    TimeSpan diff = now - logTime;
+                        DateTime logTime = DateTime.ParseExact(logTimeString, "yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+                        TimeSpan diff = now - logTime;
 
-                    //load on boot, 30 seconds should be enough
-                    //longer boot times might happen because of first time importing
-                    if (diff.TotalSeconds < 30)
-                    {
-                        EditorPrefs.SetString(key, logTimeString);
-                        update = true;
-                        Debug.Log("Loading all assets on startup.");
+                        //load on boot, 30 seconds should be enough
+                        //longer boot times might happen because of first time importing
+                        if (diff.TotalSeconds < 30)
+                        {
+                            EditorPrefs.SetString(key, logTimeString);
+                            update = true;
+                            Debug.Log("Loading all assets on startup.");
+                        }
                     }
                 }
             }
