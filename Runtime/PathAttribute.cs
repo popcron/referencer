4632bb8ce0fed87816c6f3c5e9631a3b2b146bc0
Phill <philldamaskin@gmail.com>
2020-03-14 12:45:44 -0400

    add path attribute and get path extension to enum values

---
+++ Runtime/PathAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Popcron.Referencer
+{
+    public class PathAttribute : Attribute
+    {
+        public string path;
+
+        public PathAttribute(string path)
+        {
+            this.path = path;
+        }
+    }
+}
---
+++ Runtime/ReferencesEnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Popcron.Referencer;
+using System;
+using System.Linq;
+using System.Reflection;
+
+public static class ReferencesEnumExtensions
+{
+    /// <summary>
+    /// Returns the path from a Path attribute on an enum.
+    /// </summary>
+    public static string GetPath(this Enum e)
+    {
+        Type type = e.GetType();
+        Array values = Enum.GetValues(type);
+        int index = Array.IndexOf(values, e);
+        MemberInfo[] memInfo = type.GetMember(type.GetEnumName(values.GetValue(index)));
+        if (memInfo[0].GetCustomAttributes(typeof(PathAttribute), false).FirstOrDefault() is PathAttribute pathAttribute)
+        {
+            return pathAttribute.path;
+        }
+
+        return null;
+    }
+}
